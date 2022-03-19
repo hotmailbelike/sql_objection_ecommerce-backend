c259f7a7135345426861388d2de73da06fd30911
Abdullah Omar <fishilike.fish@gmail.com>
2022-03-20 05:21:49 +0600

    feat: added seed files for products and users

--- seeds/users.js
+++ seeds/users.js
@@ -1,14 +1,46 @@
+const bcrypt = require('bcryptjs');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
 	// Deletes ALL existing entries
-	// await knex('users').del();
+	await knex('users').del();
+	const salt = await bcrypt.genSalt(10);
+
 	await knex('users').insert([
-		{ id: 1, name: 'john doe', email: 'john@gmail.com', isAdmin: false },
-		{ id: 2, name: 'jane smith', email: 'jane@gmail.com', isAdmin: false },
-		{ id: 3, name: 'olsi alaika', email: 'olsi@gmail.com', isAdmin: false },
-		{ id: 4, name: 'admin1', email: 'admin1@gmail.com', isAdmin: true },
+		{
+			id: 1,
+			name: 'john doe',
+			email: 'john@gmail.com',
+			isAdmin: false,
+			password: await bcrypt.hash('user1234', salt),
+			createdAt: new Date(),
+		},
+		{
+			id: 2,
+			name: 'jane smith',
+			email: 'jane@gmail.com',
+			isAdmin: false,
+			password: await bcrypt.hash('user1234', salt),
+			createdAt: new Date(),
+		},
+		{
+			id: 3,
+			name: 'olsi alaika',
+			email: 'olsi@gmail.com',
+			isAdmin: false,
+			password: await bcrypt.hash('user1234', salt),
+			createdAt: new Date(),
+		},
+		{
+			id: 4,
+			name: 'admin1',
+			email: 'admin1@gmail.com',
+			isAdmin: true,
+			password: await bcrypt.hash('admin1234', salt),
+			createdAt: new Date(),
+		},
 	]);
 };
